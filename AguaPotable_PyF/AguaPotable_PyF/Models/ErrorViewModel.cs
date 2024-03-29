b92fff425795d7c061cc3cc182d39281147e9983
victor <nm0343032021@unab.edu.sv>
2024-03-29 14:24:32 -0600

    New

--- AguaPotable_PyF/AguaPotable_PyF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AguaPotable_PyF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
