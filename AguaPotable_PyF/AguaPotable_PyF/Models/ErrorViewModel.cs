7cd37b39b4d55deaac0dba1806c219cca9220809
victor <nm0343032021@unab.edu.sv>
2024-03-29 14:03:54 -0600

    AguaPotable_0.1
    En este primer publish, Solo se encuentra la estructura de donde comenzaremos a programar, Lo cambie a web por que en escritorio no hay mucha informacion

---
+++ AguaPotable_PyF/AguaPotable_PyF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AguaPotable_PyF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
